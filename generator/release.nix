464893b5a4b34119b20a76a4cb3d8d63db4bb677
Jack Kelly <jack@jackkelly.name>
2020-03-31 14:42:25 +1000

    Ask for a specific version of warp

--- generator/release.nix
+++ generator/release.nix
@@ -1,5 +1,8 @@
 with import <nixpkgs> {};
-let myHaskellPackages = pkgs.haskellPackages.override {
-      overrides = self: super: with pkgs.haskell.lib;
-        { hakyll = doJailbreak (appendConfigureFlag super.hakyll "-f previewServer"); };
-    }; in myHaskellPackages.callPackage ./default.nix {}
+let
+  myHaskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: with pkgs.haskell.lib; {
+      warp = doJailbreak (super.callHackage "warp" "3.2.28" {});
+    };
+  };
+in myHaskellPackages.callPackage ./default.nix {}
