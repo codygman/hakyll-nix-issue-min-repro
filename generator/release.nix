9bd8abd3a0bbe9811a2ba5979dd5b068bdb3f081
codygman <codygman.consulting@gmail.com>
2020-03-30 23:06:45 -0500

    try jailbreaking hakyll (still gets missing warp error)

--- generator/release.nix
+++ generator/release.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 let myHaskellPackages = pkgs.haskellPackages.override {
       overrides = self: super: with pkgs.haskell.lib;
-        { hakyll = appendConfigureFlag super.hakyll "-f previewServer"; };
+        { hakyll = doJailbreak (appendConfigureFlag super.hakyll "-f previewServer"); };
     }; in myHaskellPackages.callPackage ./default.nix {}
