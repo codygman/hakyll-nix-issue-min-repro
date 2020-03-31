0532deb9f44cb577a6f4edc77083ea216e918bbc
codygman <codygman.consulting@gmail.com>
2020-03-30 23:04:39 -0500

    Revert "try using older hakyll"
    This reverts commit c901e8aeb603155ac2795251c86e5e5d48bd8f85.

--- generator/release.nix
+++ generator/release.nix
@@ -1,12 +1,5 @@
 with import <nixpkgs> {};
-let
-  oldNixPkgs = builtins.fetchTarball {
-    name = "nixos-1909-2020-03-30";
-    url = https://github.com/nixos/nixpkgs/archive/d5291756487d70bc336e33512a9baf9fa1788faf.tar.gz;
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
-  };
-  myHaskellPackages = oldNixPkgs.haskellPackages.override {
-      overrides = self: super: with oldNixPkgs.haskell.lib;
+let myHaskellPackages = pkgs.haskellPackages.override {
+      overrides = self: super: with pkgs.haskell.lib;
         { hakyll = appendConfigureFlag super.hakyll "-f previewServer"; };
     }; in myHaskellPackages.callPackage ./default.nix {}
